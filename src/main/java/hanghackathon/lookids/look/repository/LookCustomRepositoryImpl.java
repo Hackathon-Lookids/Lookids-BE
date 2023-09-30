c2667783a2470d177c4ac7e60099cc44b6c8c31c
seongwop <psw921@gmail.com>
2023-09-30 09:34:48 +0900

    fix : joinlook  ì : look 조회 쿼리 join 취소

--- src/main/java/hanghackathon/lookids/look/repository/LookCustomRepositoryImpl.java
+++ src/main/java/hanghackathon/lookids/look/repository/LookCustomRepositoryImpl.java
@@ -1,14 +1,13 @@
 package hanghackathon.lookids.look.repository;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hanghackathon.lookids.global.security.UserDetailsImpl;
 import hanghackathon.lookids.likes.LikesRepository;
+import hanghackathon.lookids.look.Look;
 import hanghackathon.lookids.look.dto.LookResponseDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
@@ -17,10 +16,8 @@ import java.util.List;
 
 import static hanghackathon.lookids.global.constant.LookType.FAMILY;
 import static hanghackathon.lookids.global.constant.LookType.KIDS;
-import static hanghackathon.lookids.likes.QLikes.likes;
 import static hanghackathon.lookids.look.QLook.look;
 
-@Slf4j
 @RequiredArgsConstructor
 public class LookCustomRepositoryImpl implements LookCustomRepository {
 
@@ -28,47 +25,24 @@ public class LookCustomRepositoryImpl implements LookCustomRepository {
     private final LikesRepository likesRepository;
 
     // slice + no offset
-//    public Slice<LookResponseDto> paginationByRandom(String type, Long lastLookId, Pageable pageable, UserDetailsImpl userDetails) {
-//        List<Look> looks = queryFactory
-//                .selectFrom(look)
-//                .where(checkType(type),
-//                        ltLookId(lastLookId))
-//                .orderBy(Expressions.numberTemplate(Double.class, "function('rand')").asc())
-//                .limit(pageable.getPageSize() + 1)
-//                .fetch();
-//
-//        // likeStatus
-//        List<LookResponseDto> result = looks.stream().map((look) -> {
-//            boolean isLiked = false;
-//            if (userDetails != null) {
-//                Long userId = userDetails.getUser().getId();
-//                isLiked = likesRepository.getLikeStatusByUserAndLook(userId, look.getId()).orElse(false);
-//            }
-//            return LookResponseDto.of(look, isLiked);
-//        }).toList();
-//
-//
-//
-//        return checkLastPage(pageable, result);
-//    }
-
     public Slice<LookResponseDto> paginationByRandom(String type, Long lastLookId, Pageable pageable, UserDetailsImpl userDetails) {
-        List<LookResponseDto> result = queryFactory
-                .select(Projections.constructor(LookResponseDto.class,
-                        look.lookType,
-                        look.imageUrls,
-                        look.title,
-                        look.content,
-                        likes.user.nickname,
-                        look.likeCount,
-                        likes.likeStatus.coalesce(false)))
-                .from(likes)
-                .leftJoin(likes.look)
+        List<Look> looks = queryFactory
+                .selectFrom(look)
                 .where(checkType(type),
                         ltLookId(lastLookId))
                 .orderBy(Expressions.numberTemplate(Double.class, "function('rand')").asc())
                 .limit(pageable.getPageSize() + 1)
                 .fetch();
+
+        // likeStatus
+        List<LookResponseDto> result = looks.stream().map((look) -> {
+            boolean isLiked = false;
+            if (userDetails != null) {
+                Long userId = userDetails.getUser().getId();
+                isLiked = likesRepository.getLikeStatusByUserAndLook(userId, look.getId()).orElse(false);
+            }
+            return LookResponseDto.of(look, isLiked);
+        }).toList();
 
         return checkLastPage(pageable, result);
     }
